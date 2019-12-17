901cb3508ed41ffb1fed72fbe50ac71b1285dee4
ShuklaShivam1994 <sshukla@lakeheadu.ca>
2019-12-16 21:39:19 -0500

    Assignment1

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
 (function () {
+'use strict';
 
-angular.module('LunchCheck', []);
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
 
-.controller('LunchCheckController', function($scope) {
-  $scope.items = "";
-  $scope.deafaultmsg = "Please enter data first";
-  $scope.displaymsg = function () {
-    var finalmsg =
-    splitString($scope.items);
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.checkDishes = function () {
+    var num = countDishes($scope.dishes);
+    $scope.message = buildMessage(num);
   };
 
-  function splitString(stringToSplit, separator) {
-  const arrayOfStrings = stringToSplit.split(separator);
-  }
-
-  const itemname = $scope.items;
-
-  const space = ',';
-  splitString(itemname, space);
-  splitString(itemname);
-  if (arrayOfStrings.length <= 2) {
-    console.log('enjoy');
+  function countDishes(dishes) {
+    var count = 0;
+    if (dishes) {
+      var array = dishes.split(',');
+      for (var idx in array) {
+        if (array[idx].trim().length != 0) {
+          count++;
+        }
+      }
     }
-    else {
-      console.log('reduce');
+    return count;
   }
 
+  function buildMessage(num) {
+    if (num === 0) {
+      return 'Please enter data first';
+    }
+    else if (num <= 3) {
+      return 'Enjoy!';
+    } else {
+      return 'Too much!';
+    }
   }
+}
 
-});
 })();
